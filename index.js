9b31d67ffec1ef3434e26a92678d8dd9d1ff056b
YohanesAgengHZP <ageng3000@gmail.com>
2024-11-05 10:47:29 +0700

    latest index

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 'use strict';
 
 require('dotenv').config();
-
 const axios = require('axios');
 const moment = require('moment');
-process.env.TZ = 'Asia/Jakarta';
-
+const fs = require('fs');
+const path = require('path');
 const TelegramBot = require('node-telegram-bot-api');
 const cron = require('node-cron');
+const winston = require('winston');
 
+process.env.TZ = 'Asia/Jakarta';
+process.env.NTBA_FIX_350 = true;
+
+const logDir = path.join(__dirname, 'log');
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: path.join(logDir, 'combined.log') }),
+        new winston.transports.File({ filename: path.join(logDir, 'errors.log'), level: 'error' })
+    ],
+});
+
+logger.exceptions.handle(
+    new winston.transports.File({ filename: path.join(logDir, 'exceptions.log') })
+);
+
+process.on('unhandledRejection', (ex) => {
+    throw ex;
+});
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
@@ -24,182 +54,238 @@ const apiUrlsOutinSLA = {
     Closed: process.env.API_Closed,
     ClosedInSLA: process.env.API_ClosedInSLA,
     ClosedOutSLA: process.env.API_ClosedOutSLA
-};
+}
 
 const apiUrlsKIP = {
     KIPOutSLA: process.env.API_KIPOutSLA
-};
+}
 
 const apiAgingKIP = {
     KIPAging: process.env.API_AgingOpenOut
-};
+}
+
+const originalFilePath = path.join(__dirname, 'sample.csv');
 
 function getPreviousDaysDates(days) {
-    const endDate = moment().subtract(1, 'days').endOf('day');
-    const startDate = moment('2024-01-01');
+    const endDate = moment().subtract(1, 'days').endOf('day').format('YYYY-MM-DD HH:mm'); 
+    const startDate = moment('2024-01-01 00:00').format('YYYY-MM-DD HH:mm'); 
     return { startDate, endDate };
 }
 
-async function fetchWithRetry(url, data, retries = 3, delay = 1000, timeout = 120000) {
-    console.time(`fetchWithRetry ${url}`);
-    for (let i = 0; i < retries; i++) {
-        try {
-            const response = await axios.post(url, data, { timeout });
-            console.timeEnd(`fetchWithRetry ${url}`);
-            return response;
-        } catch (error) {
-            if (i === retries - 1 || !error.response || error.response.status !== 504) {
-                console.timeEnd(`fetchWithRetry ${url}`);
-                throw error;
-            }
-            await new Promise(resolve => setTimeout(resolve, delay * Math.pow(2, i)));
-        }
-    }
-}
-
-async function sendCombinedReport() {
-    console.time('sendCombinedReport');
+async function sendCombinedReport(chatId) {
     try {
         const { startDate, endDate } = getPreviousDaysDates(30);
-        const startDateFormat = startDate.format('D MMM YYYY');
-        const endDateFormat = endDate.format('D MMM YYYY');
-        const reportDateRange = `${startDateFormat} - ${endDateFormat}`;
 
-        const formattedStartDate = startDate.format('YYYY-MM-DD');
-        const formattedEndDate = endDate.format('YYYY-MM-DD');
+        const reportDateRange = `${startDate} - ${endDate}`;
+        const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
+        logger.info(`[${timestamp}] Sending API request to Unclosed with parameters: start_date=${startDate}, end_date=${endDate}`);
 
-        console.time('fetch ticket data');
-        const ticketDataPromises = [
-            fetchWithRetry(apiUrlsOpeninSLA.Unclosed, { start_date: formattedStartDate, end_date: formattedEndDate }),
-            fetchWithRetry(apiUrlsOpeninSLA.OpenInSLA, { start_date: formattedStartDate, end_date: formattedEndDate }),
-            fetchWithRetry(apiUrlsOpeninSLA.OpenOutSLA, { start_date: formattedStartDate, end_date: formattedEndDate })
-        ];
-
-        const [unclosedResponse, openInSLAResponse, openOutSLAResponse] = await Promise.all(ticketDataPromises);
-        console.timeEnd('fetch ticket data');
-
-        const ticketData = `
-<b>• Ticket - Unclosed:</b> ${unclosedResponse.data.data[0].count_id}
-<b>• Ticket - Open In SLA</b>: ${openInSLAResponse.data.data[0].count_id}
-<b>• Ticket - Open Out SLA</b>: ${openOutSLAResponse.data.data[0].count_id}
-`;
-
-        console.time('fetch KIP data');
-        const kipResponse = await fetchWithRetry(apiUrlsKIP.KIPOutSLA, {
-            channel: "ALL",
-            start_date: formattedStartDate,
-            end_date: formattedEndDate
+        const unclosedResponse = await axios.post(apiUrlsOpeninSLA.Unclosed, {
+            start_date: startDate,
+            end_date: endDate
         });
-        console.timeEnd('fetch KIP data');
 
-        const delayedData = kipResponse.data.data.delayed;
-        const slaMapping = { sla_1: '1HK', sla_3: '3HK', sla_7: '7HK', sla_14: '14HK' };
-        const sortedSLAKeys = Object.keys(delayedData)
-            .filter(sla => sla !== 'sla_7')
-            .sort((a, b) => parseInt(slaMapping[b].match(/\d+/)[0]) - parseInt(slaMapping[a].match(/\d+/)[0]));
+        logger.info(`[${timestamp}] Sending API request to OpenInSLA with parameters: start_date=${startDate}, end_date=${endDate}`);
+        const openInSLAResponse = await axios.post(apiUrlsOpeninSLA.OpenInSLA, {
+            start_date: startDate,
+            end_date: endDate
+        });
+
+        logger.info(`[${timestamp}] Sending API request to OpenOutSLA with parameters: start_date=${startDate}, end_date=${endDate}`);
+        const openOutSLAResponse = await axios.post(apiUrlsOpeninSLA.OpenOutSLA, {
+            start_date: startDate,
+            end_date: endDate
+        });
+
+        logger.info(`[${timestamp}] Received response for Unclosed: ${JSON.stringify(unclosedResponse.data)}`);
+        logger.info(`[${timestamp}] Received response for OpenInSLA: ${JSON.stringify(openInSLAResponse.data)}`);
+        logger.info(`[${timestamp}] Received response for OpenOutSLA: ${JSON.stringify(openOutSLAResponse.data)}`);
+
+        const unclosedCount = unclosedResponse.data.data[0].count_id;
+        const openInSLACount = openInSLAResponse.data.data[0].count_id;
+        const openOutSLACount = openOutSLAResponse.data.data[0].count_id;
+
+        const ticketData = `• <b>Ticket - Unclosed : </b>${unclosedCount}\n• <b>Ticket - Open In SLA : </b>${openInSLACount}\n• <b>Ticket - Open Out SLA : </b>${openOutSLACount}\n`;
 
         let kipData = '';
+
+        logger.info(`[${timestamp}] Sending API request to KIP with parameters: channel=ALL, start_date=${startDate}, end_date=${endDate}`);
+        const kipResponse = await axios.post(apiUrlsKIP.KIPOutSLA, {
+            channel: "ALL",
+            start_date: startDate,
+            end_date: endDate
+        });
+
+        logger.info(`[${timestamp}] Received response for KIP: ${JSON.stringify(kipResponse.data)}`);
+
+        const delayedData = kipResponse.data.data.delayed;
+
+        const slaMapping = {
+            sla_1: '1HK',
+            sla_3: '3HK',
+            sla_7: '7HK',
+            sla_14: '14HK'
+        };
+
+        const customSort = (a, b) => {
+            const keyA = slaMapping[a] || a;
+            const keyB = slaMapping[b] || b;
+            const numA = parseInt(keyA.match(/\d+/)[0]);
+            const numB = parseInt(keyB.match(/\d+/)[0]);
+
+            if (numA < numB) return 1;
+            if (numA > numB) return -1;
+            return 0;
+        };
+
+        const sortedSLAKeys = Object.keys(delayedData)
+            .filter(sla => sla !== 'sla_7')
+            .sort(customSort);
+
         for (const sla of sortedSLAKeys) {
             const slaKey = slaMapping[sla] || sla;
-            kipData += `<b>TOP 3 KIP out of SLA ${slaKey}:</b>\n`;
+            kipData += `<b>TOP 3 KIP out of SLA ${slaKey}:\n</b>`;
 
             const sortedData = Object.entries(delayedData[sla].data).sort((a, b) => b[1] - a[1]);
-            sortedData.slice(0, 3).forEach(item => {
+            const top3Items = sortedData.slice(0, 3);
+
+            top3Items.forEach(item => {
                 kipData += `• ${item[0]}: ${item[1]}\n`;
             });
-            kipData += '';
+            kipData += '\n';
         }
 
-        console.time('fetch closing ticket data');
-        const closedDataPromises = [
-            fetchWithRetry(apiUrlsOutinSLA.Closed, { start_date: formattedStartDate, end_date: formattedEndDate }),
-            fetchWithRetry(apiUrlsOutinSLA.ClosedInSLA, { start_date: formattedStartDate, end_date: formattedEndDate }),
-            fetchWithRetry(apiUrlsOutinSLA.ClosedOutSLA, { start_date: formattedStartDate, end_date: formattedEndDate })
-        ];
+        logger.info(`[${timestamp}] Sending API request to Closed with parameters: start_date=${startDate}, end_date=${endDate}`);
+        const closedResponse = await axios.post(apiUrlsOutinSLA.Closed, {
+            start_date: startDate,
+            end_date: endDate
+        });
 
-        const [closedResponse, closedInSLAResponse, closedOutSLAResponse] = await Promise.all(closedDataPromises);
-        console.timeEnd('fetch closing ticket data');
+        logger.info(`[${timestamp}] Sending API request to ClosedInSLA with parameters: start_date=${startDate}, end_date=${endDate}`);
+        const closedInSLAResponse = await axios.post(apiUrlsOutinSLA.ClosedInSLA, {
+            start_date: startDate,
+            end_date: endDate
+        });
 
-        const closingTicketData = `
-<b>• Ticket - Closed</b>: ${closedResponse.data.data[0].count_id}
-<b>• Ticket - Closed In SLA</b>: ${closedInSLAResponse.data.data[0].count_id}
-<b>• Ticket - Closed Out SLA</b>: ${closedOutSLAResponse.data.data[0].count_id}
-`;
+        logger.info(`[${timestamp}] Sending API request to ClosedOutSLA with parameters: start_date=${startDate}, end_date=${endDate}`);
+        const closedOutSLAResponse = await axios.post(apiUrlsOutinSLA.ClosedOutSLA, {
+            start_date: startDate,
+            end_date: endDate
+        });
 
-        const reportString = `
--------------------------------
-<b>Report ${reportDateRange}</b>
--------------------------------
-${ticketData}
--------------------------------
-${kipData}
--------------------------------
-${closingTicketData}
-`;
+        logger.info(`[${timestamp}] Received response for Closed: ${JSON.stringify(closedResponse.data)}`);
+        logger.info(`[${timestamp}] Received response for ClosedInSLA: ${JSON.stringify(closedInSLAResponse.data)}`);
+        logger.info(`[${timestamp}] Received response for ClosedOutSLA: ${JSON.stringify(closedOutSLAResponse.data)}`);
 
+        const closedCount = closedResponse.data.data[0].count_id;
+        const closedInSLACount = closedInSLAResponse.data.data[0].count_id;
+        const closedOutSLACount = closedOutSLAResponse.data.data[0].count_id;
+
+        const closingTicketData = `<b>• Ticket - Closed : </b>${closedCount}\n<b>• Ticket - Closed In SLA : </b>${closedInSLACount}\n<b>• Ticket - Closed Out SLA : </b>${closedOutSLACount}\n`;
+
+        let reportString = `-------------------------------\n<b>Report ${reportDateRange}</b>\n-------------------------------\n`;
+
+        reportString += '\n';
+        reportString += ticketData;
+        reportString += '\n';
+        reportString += '-------------------------------\n';
+        reportString += '\n';
+        reportString += kipData;
+        reportString += '-------------------------------\n';
+        reportString += '\n';
+        reportString += closingTicketData;
+
+        const response = await axios.post('https://crm.linkaja.id/svc/report/ticket-monitor/ticket-open-out-sla', {
+            type: 'telegram',
+            start_date: startDate,
+            end_date: endDate,
+        });
+
+        if (response.data.success) {
+            const fileUrl = response.data.fileurl; 
+            const fileLink = `Download the detailed report <a href="${fileUrl}">here</a> (generated on ${timestamp}).`;
+
+            reportString += `-------------------------------\n`;
+            reportString += `${fileLink}\n`;
+            reportString += `-------------------------------\n`;
+        } else {
+            reportString += 'Failed to fetch the detailed report data.\n';
+        }
+        
         bot.sendMessage(chatId, reportString, { parse_mode: 'HTML' });
     } catch (error) {
-        bot.sendMessage(chatId, `Error fetching data: ${error.message}`);
+        logger.error(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Error generating report: ${error.message}`);
+        bot.sendMessage(chatId, `Error generating report: ${error.message}`);
     }
-    console.timeEnd('sendCombinedReport');
 }
 
 async function top3DetailsReport() {
-    console.time('top3DetailsReport');
     try {
         const { startDate, endDate } = getPreviousDaysDates(30);
-        const startDateFormat = startDate.format('D MMM YYYY');
-        const endDateFormat = endDate.format('D MMM YYYY');
-        const reportDateRange = `${startDateFormat} - ${endDateFormat}`;
+        const reportDateRange = `${startDate} - ${endDate}`;
 
-        const formattedStartDate = startDate.format('YYYY-MM-DD');
-        const formattedEndDate = endDate.format('YYYY-MM-DD');
-
-        console.time('fetch top 3 details');
-        const response = await fetchWithRetry(apiAgingKIP.KIPAging, {
-            start_date: formattedStartDate,
-            end_date: formattedEndDate,
+        const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
+        logger.info(`[${timestamp}] Sending API request to KIPAging with parameters: start_date=${startDate}, end_date=${endDate}, channel=ALL`);
+        
+        const response = await axios.post(apiAgingKIP.KIPAging, {
+            start_date: startDate,
+            end_date: endDate,
             channel: 'ALL'
         });
-        console.timeEnd('fetch top 3 details');
+        
+        const data = response.data.data;
 
-        const data = response.data.data.sort((a, b) => b.total_ticket - a.total_ticket);
-        let top3Report = `
--------------------------------
-<b>Aging cluster for ticket open out of SLA
-${reportDateRange}</b>
--------------------------------
-`;
+        // sort data
+        data.sort((a, b) => b.total_ticket - a.total_ticket);
 
+        let top3Report = '';
+        top3Report += `-------------------------------\n`
+        top3Report += `<b>Aging cluster for ticket open out of SLA </b>\n`;
+        top3Report += `<b>${reportDateRange}</b>\n`;
+        top3Report += `-------------------------------\n\n`
+
+        // Display the top 3 entries
         for (let i = 0; i < Math.min(data.length, 3); i++) {
             const entry = data[i];
-            const agingRanges = {
-                'Aging 3-7': 0,
-                'Aging 8-14': 0,
-                'Aging 15-20': 0,
-                'Aging 21-30': 0,
-                'Aging >30': 0
-            };
+            top3Report += `<b> • ${entry.kip_2}</b>\n`;
+            top3Report += `Total Tickets: ${entry.total_ticket}\n`;
 
-            Object.keys(agingRanges).forEach(range => {
-                const [start, end] = range.match(/\d+/g) || [];
-                for (let j = +start || 0; j <= (+end || 100); j++) {
-                    agingRanges[range] += entry[`aging_${j}`] || 0;
-                }
-            });
+            let aging_3_7 = 0;
+            let aging_8_14 = 0;
+            let aging_15_20 = 0;
+            let aging_21_30 = 0;
+            let aging_greater_30 = 0;
 
-            top3Report += `
-<b>• ${entry.kip_2}</b>
-Total Tickets: ${entry.total_ticket}
-${Object.entries(agingRanges).map(([range, count]) => `${range}: ${count}`).join('\n')}
-`;
+            // TOP 1  KIP
+            for (let j = 3; j <= 7; j++) {
+                aging_3_7 += entry[`aging_${j}`] || 0;
+            }
+            for (let j = 8; j <= 14; j++) {
+                aging_8_14 += entry[`aging_${j}`] || 0;
+            }
+            for (let j = 15; j <= 20; j++) {
+                aging_15_20 += entry[`aging_${j}`] || 0;
+            }
+            for (let j = 21; j <= 30; j++) {
+                aging_21_30 += entry[`aging_${j}`] || 0;
+            }
+            for (let j = 31; j <= 100; j++) {
+                aging_greater_30 += entry[`aging_${j}`] || 0;
+            }
+
+            top3Report += `<b>Details :</b>\n`;
+            top3Report += `Aging 3-7 : ${aging_3_7}\n`;
+            top3Report += `Aging 8-14 : ${aging_8_14}\n`;
+            top3Report += `Aging 15-20 : ${aging_15_20}\n`;
+            top3Report += `Aging 21-30 : ${aging_21_30}\n`;
+            top3Report += `Aging >30 : ${aging_greater_30}\n\n`;
         }
 
         bot.sendMessage(chatId, top3Report, { parse_mode: 'HTML' });
     } catch (error) {
-        bot.sendMessage(chatId, `Error fetching data: ${error.message}`);
+        logger.error(`[${timestamp}] Error fetching top 3 details report: ${error.message}`);
+        console.error('Error fetching top 3 details report:', error.message);
     }
-    console.timeEnd('top3DetailsReport');
 }
 
 bot.onText(/\/start/, async (msg) => {
@@ -213,11 +299,13 @@ bot.onText(/\/start/, async (msg) => {
     }
 });
 
+// Cron at 8 AM
 cron.schedule('0 8 * * *', async () => {
+    bot.sendMessage(chatId, 'Scheduled report is being sent now.');
     try {
-        await sendCombinedReport();
-        await top3DetailsReport();
+        await sendCombinedReport(chatId);
+        await top3DetailsReport(chatId);
     } catch (error) {
-        console.error('Error in sending combined report:', error.message);
+        console.error('Error in sending scheduled report:', error.message);
     }
 });
